d3f8b50e624ad5c2b2eed1e46116c66b7189db89
Simon Solts <simon.solts@bark.com>
2023-01-20 19:10:54 +0000

    From docs

--- app/connect.py
+++ app/connect.py
@@ -1,19 +1,16 @@
-#!/usr/bin/python3
-
 import bluetooth
+from bluetooth.ble import DiscoveryService
 
-def scan():
-    print("Scanning for bluetooth devices:")
-    devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
-    number_of_devices = len(devices)
-    print(number_of_devices,"devices found")
-    for addr, name, device_class in devices:
-        print("\n")
-        print("Device:")
-        print("Device Name: %s" % (name))
-        print("Device MAC Address: %s" % (addr))
-        print("Device Class: %s" % (device_class))
-        print("\n")
-    return
 
-scan()
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("Found {} devices.".format(len(nearby_devices)))
+
+for addr, name in nearby_devices:
+    print("  {} - {}".format(addr, name))
+    
+
+service = DiscoveryService()
+devices = service.discover(2)
+
+for address, name in devices.items():
+    print("name: {}, address: {}".format(name, address))
