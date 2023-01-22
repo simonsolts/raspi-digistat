aa4dcdcf3b5bc61908999d8f0cf2edf80d892fff
Simon Solts <simon.solts@bark.com>
2023-01-22 15:30:23 +0000

    Config

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'Digistat Controller';
+export const PLATFORM_NAME = 'Digistat';
 
 /**
  * This must match the name of your plugin as defined the package.json
